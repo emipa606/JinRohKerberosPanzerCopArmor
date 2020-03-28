e1a9b0ed996b96b85b9d45d4a1295369b205814a
Mlie <emipa606@users.noreply.github.com>
2020-03-28 21:51:02 +0100

    First publish

---
+++ Source/JinRohArmor/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyTitle("JinRohArmor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JinRohArmor")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("cc6dc590-fe12-428c-b0aa-e599bffa3d42")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
